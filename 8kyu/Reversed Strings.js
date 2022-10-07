92db8d0edaa6ca9393472951310451d6d8fc39af
Alexander <Starnovskiy97@gmail.com>
2022-10-07 21:31:42 +0900

    codewars challenge

---
+++ 8kyu/Reversed Strings.js
@@ -0,0 +1,14 @@
+//DESCRIPTION:
+//Complete the solution so that it reverses the string passed into it.
+
+//'world'  =>  'dlrow'
+//'word'   =>  'drow'
+
+//Мое решение:
+function solution(str) {
+   let reverse = "";
+   for (let i = str.length - 1; i >= 0; i--) {
+      reverse += str[i];
+   }
+   return reverse;
+}
