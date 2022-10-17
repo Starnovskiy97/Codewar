5af74fc7c1a2b3fd9b557aef5cac589158bb0088
Alexander <Starnovskiy97@gmail.com>
2022-10-17 11:24:39 +0900

    codewars challenge

---
+++ 7kyu/Credit Card Mask.js
@@ -0,0 +1,31 @@
+//DESCRIPTION:
+//Usually when you buy something, you're asked whether your credit card number,
+//phone number or answer to your most secret question is still correct.However,
+//since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+//Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+//	Examples
+//"4556364607935616" -- > "############5616"
+//"64607935616" -- > "#######5616"
+//"1" -- > "1"
+//"" -- > ""
+
+//// "What was the name of your first pet?"
+
+//"Skippy" -- > "##ippy"
+
+//"Nananananananananananananananana Batman!"
+//-->
+//	"####################################man!"
+
+
+
+//Мое решение:
+function maskify(cc) {
+	let result = cc.split('');
+	for (let i = 0; i < (result.length - 4); i++) {
+		result.splice(i, 1, '#');
+	}
+	return result.join('');
+}
---
+++ 7kyu/Shortest Word.js
@@ -0,0 +1,12 @@
+//DESCRIPTION:
+//Simple, given a string of words, return the length of the shortest word(s).
+
+//String will never be empty and you do not need to account for different data types.
+
+
+
+//Мое решение:
+function findShort(s) {
+	let result = s.split(' ').sort((a, b) => a.length - b.length);
+	return result[0].length;
+}
